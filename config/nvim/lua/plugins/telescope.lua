87fdf156c56e1ef4354b01268bd8b66f0831772f
Guilherme Hilst Ribeiro <guilhermehilst@gmail.com>
2025-11-19 14:19:29 -0300

    Adiciona FZF na busca por arquivos no NeoVim utilizando Telescope e atualiza lazy-lock.json

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+      }
+    },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files Telescope" },
       { "<C-p>", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find Files Telescope" },
@@ -19,13 +25,29 @@ return {
         find_files = {
           -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
           hidden = true,
-          find_command = { "rg", "--files", "--ignore-case", "--glob", "!**/.git/*", "-L" },
+          -- find_command = { "rg", "--files", "--hidden", "--ignore-case", "--glob", "!**/.git/*", "-L" },
+          find_command = { "rg", "--files", "--hidden", "--ignore-case", "--glob", "!**/.git/*", "-L" },
         },
       },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          case_mode = "ignore_case",        -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
+      },
     },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
+    end,
   },
+
   {
     "nvim-telescope/telescope-symbols.nvim",
+    keys = {
+      { "<leader>fe", "<cmd>lua require('telescope.builtin').symbols{ sources = {'emoji', 'gitmoji'} }<CR>", desc = "Find Emojis Telescope" },
+    }
   },
 
   -- Custom ripgrep configuration:
